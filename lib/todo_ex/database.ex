d34d8715e2fc128c1df0c3d56c1d1e4577e8882f
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-07-16 21:14:26 -0300

    Turns database into a supervisor of database workers

--- lib/todo_ex/database.ex
+++ lib/todo_ex/database.ex
@@ -1,49 +1,39 @@
 defmodule TodoEx.Database do
-  use GenServer
 
   @default_num_workers 5
   @default_db_folder "./persist"
 
   def start_link(opts \\ %{}) do
     opts = set_defaults(opts)
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def init(%{db_folder: db_folder}) do
+    IO.puts("Starting database server.")
+    File.mkdir_p!(db_folder)
+
+    children = Enum.map(0..@default_num_workers, fn id -> worker_spec(id, db_folder) end)
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :supervisor
+    }
   end
 
   defp set_defaults(opts) do
-    num_workers = Map.get(opts, :num_workers, @default_num_workers)
     db_folder = Map.get(opts, :db_folder, @default_db_folder)
 
     opts
-    |> Map.put(:num_workers, num_workers)
     |> Map.put(:db_folder, db_folder)
   end
 
-  @impl GenServer
-  def init(opts = %{num_workers: num_workers})
-      when is_number(num_workers) do
-    IO.puts("Starting database server.")
-    {:ok, opts, {:continue, :start_workers}}
-  end
-
-  @impl GenServer
-  def handle_continue(:start_workers, state = %{num_workers: num_workers, db_folder: db_folder}) do
-    File.mkdir_p!(db_folder)
-
-    Enum.each(0..num_workers, fn index ->
-      {:ok, _pid} = TodoEx.DatabaseWorker.start_link(%{id: index, db_folder: db_folder})
-    end)
-
-    {:noreply, state}
-  end
-
-  @impl GenServer
-  def handle_call(
-        {:choose_worker, key},
-        _from,
-        state = %{num_workers: num_workers}
-      ) do
-    worker_id = :erlang.phash2(key, num_workers)
-    {:reply, worker_id, state}
+  defp worker_spec(id, db_folder) do
+    default_worker_spec = {TodoEx.DatabaseWorker, %{id: id, db_folder: db_folder}}
+    Supervisor.child_spec(default_worker_spec, id: id)
   end
 
   def store(key, data) do
@@ -59,6 +49,6 @@ defmodule TodoEx.Database do
   end
 
   defp choose_worker(key) do
-    GenServer.call(__MODULE__, {:choose_worker, key})
+    :erlang.phash2(key, @default_num_workers)
   end
 end
